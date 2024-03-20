a4e4a594553e546a341b4529dc3f3169e043f712
Adam Olsen <arolsen@gmail.com>
2024-03-20 09:23:23 -0600

    feat: add dummy setup function

---
+++ lua/telescope-dirpicker.lua
@@ -0,0 +1,8 @@
+local M = {}
+
+M.setup = function()
+	-- not needed (yet), but added a setup function here so it doesn't break if
+	-- someone puts `opts = {}` or `config = true` in their lazy.nvim plugin spec
+end
+
+return M
